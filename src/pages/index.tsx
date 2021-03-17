24dc040c3a3c4351a6e388084e3b456e5343cbb0
AK-10 <atsushi524k10@gmail.com>
2021-03-18 06:13:54 +0900

    ts and tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,12 @@
 import React from "react"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
     </div>
   </Layout>
 )
