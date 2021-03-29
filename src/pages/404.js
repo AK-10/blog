9ab6348ae71c29b56c9850ea50a8d1dccdb46f04
AK-10 <atsushi524k10@gmail.com>
2021-03-30 08:32:27 +0900

    tweak 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,8 +6,16 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1
+      className="text-2xl sm:text-3xl md:text-4xl font-medium text-navy-700"
+    >
+      404: Not Found
+    </h1>
+    <p
+      className="mt-3 text-navy-500"
+    >
+      残念！ないものはないよ！
+    </p>
   </Layout>
 )
 
